c4183ed01082d66011f14b109467b04454a5415d
maxx <max.kjellman@gmail.com>
2019-04-25 12:06:04 +0200

    committing new features

---
+++ depositeMoney/src/main/java/com/helpmeout/depositeMoney/DepositeMoneyApplication.java
@@ -0,0 +1,13 @@
+package com.helpmeout.depositeMoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepositeMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepositeMoneyApplication.class, args);
+	}
+
+}
---
+++ depositeMoney/src/test/java/com/helpmeout/depositeMoney/DepositeMoneyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.helpmeout.depositeMoney;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DepositeMoneyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
