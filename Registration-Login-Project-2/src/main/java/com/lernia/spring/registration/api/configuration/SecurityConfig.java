8867a2a777f691a89603238bd93c868195e0c5e7
localadmin <localadmin@selenium>
2019-05-16 23:02:14 +0200

    Just test to see if git works.

--- Registration-Login-Project-2/src/main/java/com/lernia/spring/registration/api/configuration/SecurityConfig.java
+++ Registration-Login-Project-2/src/main/java/com/lernia/spring/registration/api/configuration/SecurityConfig.java
@@ -1,13 +1,8 @@
 package com.lernia.spring.registration.api.configuration;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
